8db75b5dd462ae3d15912b126dc7770c3841e912
Kathyellen Souza <87395970+souzakathyellen@users.noreply.github.com>
2022-10-25 16:40:49 -0300

    Add files via upload

---
+++ OperadoresAritmeticos/OperadoresAritmeticos/src/OperadoresAritmeticos.java
@@ -0,0 +1,32 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) throws Exception {
+     /* int n1 = 3;
+    int n2 = 5;
+
+    float m = (n1 + n2)/2;
+    System.out.println("A média é igual a : " + m );
+    */
+
+  /*  int numero = 10;
+    int valor = 4 + -- numero;
+    System.out.println(valor);
+    System.out.println(numero);
+   */
+  
+  /* int x = 4;
+   x += 2;
+   System.out.println(x);     
+  */
+
+    /*Float v = 2.8f;
+    int ar = (int) Math.round(v);
+    System.out.println(ar);
+   */
+  
+   double ale = Math.random();
+   int n = (int) (15 + ale * (50-15));
+   System.out.println(n);
+    
+
+    }
+}
